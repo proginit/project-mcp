43ce776897a4d9ec67c1a95751d3cb988a1d364f
Neiber Vasquez Carlos <neiber.vc.prog@gmail.com>
2025-10-26 00:37:37 -0400

    change var

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,8 @@
-export function multiply(a,b){
+export function mult(a,b){
     return a*b
 }
 
-export function divide(a,b){
+export function div(a,b){
     if(b==0) return 0
     else return a/b
 }
