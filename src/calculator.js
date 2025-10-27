d90e821a4f7be3e03e822b1e7f51e5d0b6e0a2be
Neiber Vasquez Carlos <neiber.vc.prog@gmail.com>
2025-10-27 15:25:33 -0400

    cambion de nombres

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,8 @@
-export function metodo(num1, num2) {
+export function multiply(num1, num2) {
   return num1 * num2;
 }
 
-export function metodo2(num1, num2) {
+export function division(num1, num2) {
   if (num2 == 0) return 0;
   else return num1 / num2;
 }
