1d1957c87a77370c082f882be6df8c81d9e29143
Neiber Vasquez Carlos <neiber.vc.prog@gmail.com>
2025-10-27 02:04:56 -0400

    change name

--- src/calculator.js
+++ src/calculator.js
@@ -2,7 +2,7 @@ export function metodo1(num1,num2){
     return num1*num2
 }
 
-export function metodo2(num1,num2){
+export function div11(num1,num2){
     if(num2==0) return 0
     else return num1/num2
 }
