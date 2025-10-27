c0a71fe71341ce7889bafb4ab77598ed62e84e68
Neiber Vasquez Carlos <neiber.vc.prog@gmail.com>
2025-10-27 14:45:48 -0400

    delete file

--- src/promedio.js
+++
@@ -1,9 +0,0 @@
-const notasEntrada11 = [85, 90, 78, 92];
-let sumaNotas11 = 0;
-
-for (let i = 0; i < notasEntrada.length; i++) {
-  sumaNotas11 += notasEntrada11[i];
-}
-
-const promedio = sumaNotas11 / notasEntrada11.length;
-console.log("Promedio Total: ", promedio);
