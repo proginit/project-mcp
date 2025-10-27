e835f6b405b6e1e19efc27fa8fdf64f9d4c94664
Neiber Vasquez Carlos <neiber.vc.prog@gmail.com>
2025-10-27 01:45:17 -0400

    example

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,8 @@
-export function multiplica111(num1,num2){
+export function multiplica11(num1,num2){
     return num1*num2
 }
 
-export function divide111(num1,num2){
+export function divide11(num1,num2){
     if(num2==0) return 0
     else return num1/num2
 }
