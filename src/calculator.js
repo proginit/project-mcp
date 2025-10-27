61ee1154974588aa85a695ad04e2bc95baa5221e
Neiber Vasquez Carlos <neiber.vc.prog@gmail.com>
2025-10-27 01:01:26 -0400

    change name

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,8 @@
-export function multiplica(num1,num2){
+export function multiplica1(num1,num2){
     return num1*num2
 }
 
-export function divide(num1,num2){
+export function divide1(num1,num2){
     if(num2==0) return 0
     else return num1/num2
 }
