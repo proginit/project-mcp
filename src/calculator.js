f33b13f50491e995927d0971571fc3a7924dd553
Neiber Vasquez Carlos <neiber.vc.prog@gmail.com>
2025-10-27 02:29:45 -0400

    name

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,8 @@
-export function metodo1(num1, num2) {
+export function metodo11(num1, num2) {
   return num1 * num2;
 }
 
-export function metodo2(num1, num2) {
+export function metodo22(num1, num2) {
   if (num2 == 0) return 0;
   else return num1 / num2;
 }
